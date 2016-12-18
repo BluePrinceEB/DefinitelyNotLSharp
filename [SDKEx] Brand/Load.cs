656d5e64e763a76052bbf2030a2979bc33a7f021
BluePrinceEB <primeworld010@mail.ru>
2016-12-18 19:03:15 +0500

    Add files via upload

---
+++ [SDKEx] Brand/Load.cs
@@ -0,0 +1,26 @@
+﻿namespace Brand
+{
+    using System;
+    using LeagueSharp.SDK;
+
+    using static Extensions.Config;
+
+    class Load
+    {
+        static void Main(string[] args)
+        {
+            Bootstrap.Init(args);
+            Events.OnLoad += OnLoad;
+        }
+
+        private static void OnLoad(object sender, EventArgs eventArgs)
+        {
+            if (MyHero.ChampionName == "Brand")
+            {
+                Extensions.Events.Initialize();
+                Extensions.Config.Initialize();
+                Extensions.Spells.Initialize();
+            }
+        }
+    }
+}
---
+++ [SDKEx] Jayce/Load.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------  
+// <summary>
+//   The Load.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Jayce
+{
+    #region
+
+    using System;
+
+    using LeagueSharp;
+    using LeagueSharp.SDK;
+
+    using Events = LeagueSharp.SDK.Events;
+
+    #endregion
+
+    /// <summary>
+    ///     The Load.
+    /// </summary>
+    internal class Load
+    {
+        #region Methods
+
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            Bootstrap.Init(args);
+            Events.OnLoad += OnLoad;
+        }
+
+        /// <summary>
+        /// The on load.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="eventArgs">
+        /// The event args.
+        /// </param>
+        private static void OnLoad(object sender, EventArgs eventArgs)
+        {
+            if (ObjectManager.Player.ChampionName == "Jayce")
+            {
+                Extensions.Events.Initialize();
+                Extensions.Spells.Initialize();
+                Extensions.Config.Initialize();
+            }
+        }
+
+        #endregion
+    }
+}
